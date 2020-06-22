daab18ceb95c824ea77f74bfd9dd6779a1eec70c
Kamil Hosaja <61997428+vorayo@users.noreply.github.com>
2020-06-22 19:18:36 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -45,17 +45,16 @@ def get_mask_on_img(img, mask):
     mask - predicted mask (uint with values for different elements or True and False in case of binary)
     Returns: rgb image with mask on image (red - tumour, green - kidney)
     '''
-    from skimage.color import gray2rgb
-    rgb_img = gray2rgb(img)
+    from starter_code.visualize import hu_to_grayscale    
+    
+    rgb_img = hu_to_grayscale(img,-512,512)/255.
 
-    ##
-
-    mask_slice_tum = np.where(mask == 1, 1, 0)
-    mask_slice_kid = np.where(mask == 2, 1, 0)
+    mask_slice = np.where(mask == 1, 1, 0)
+#     mask_slice_kid = np.where(mask == 2, 1, 0) # could be developed in order to plot both tumor and kidney masks on one image.
 
     rgb_mask = np.zeros((mask.shape[0], mask.shape[1], 3))
-    rgb_mask[:, :, 0] = mask_slice_tum
-    rgb_mask[:, :, 1] = mask_slice_kid
+    rgb_mask[:, :, 0] = mask_slice
+#     rgb_mask[:, :, 1] = mask_slice_kid
 
     return rgb_img + rgb_mask
 
@@ -73,7 +72,7 @@ def show_subplot_results(img, mask, mask_pred, path=None):
     mask_orig_on_img = get_mask_on_img(img, mask)
     mask_pred_on_img = get_mask_on_img(img, mask_pred)
 
-    plt.figure(num=1, dpi=300)
+    plt.figure(dpi=300)
     plt.subplot(131)
     plt.imshow(mask_orig_on_img, cmap='gray')
     plt.title("Original image\nwith original mask", fontsize=10)
